1818642e6c361df40bdd14a8d7d2c1cb494ceb12
renathossain <renathossain@gmail.com>
2025-05-11 14:07:48 -0400

    Added wanted programs.

---
+++ postinstall.ps1
@@ -0,0 +1,13 @@
+scoop bucket add extras
+scoop install extras/firefox
+scoop install extras/ungoogled-chromium
+scoop install extras/discord
+scoop install extras/glazewm
+scoop install extras/mpv
+scoop install extras/okular
+scoop install extras/onlyoffice-desktopeditors
+scoop install extras/crystaldiskinfo
+scoop install extras/crystaldiskmark
+
+scoop bucket add versions
+scoop install versions/steam
