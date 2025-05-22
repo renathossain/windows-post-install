9db1c84159b9e39d673b1dfacbb26dd0b40b5182
renathossain <114009484+renathossain@users.noreply.github.com>
2025-05-22 14:52:08 -0400

    Update postinstall.ps1

--- postinstall.ps1
+++ postinstall.ps1
@@ -16,6 +16,7 @@ winget install ONLYOFFICE.DesktopEditors
 winget install 9P3JFR0CLLL6 # mpv Video Player
 winget install KDE.Okular
 winget install Valve.Steam
+winget install EpicGames.EpicGamesLauncher
 winget install CrystalDewWorld.CrystalDiskInfo
 winget install CrystalDewWorld.CrystalDiskMark
 winget upgrade --all
